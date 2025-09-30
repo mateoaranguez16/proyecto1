b73712763f30b94918084cc82b07b4e12a131c5e
Alejandro Sarmiento Suarez <159832434+AlejandroSarmientoSuarez@users.noreply.github.com>
2025-09-29 21:20:55 -0300

    Delete proyecto1

--- proyecto1/src/pages/js/register.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('registro-form').addEventListener('submit', function(e) {
-  const password = document.getElementById('password').value;
-  const confirmPassword = document.getElementById('confirm-password').value;
-
-  if (password !== confirmPassword) {
-    e.preventDefault();
-    alert('Las contraseñas no coinciden.');
-  }
-});
